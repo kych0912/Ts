2183e3b72064a1e26cc777d9cbfcf0d75a08d8b9
YoungCheon Kim <kyt031522@gmail.com>
2024-01-31 04:11:22 +0900

    narrowing, type, function

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var 이름 = { name: "kim" };
+var 이름 = { grade: "A" };
 var 이름2 = 123;
 var 이름3 = 123;
 function 함수(x) {
@@ -13,4 +13,33 @@ var User = /** @class */ (function () {
     return User;
 }());
 var 회원들 = ["kim", "park", "lee", 123, 456, 789];
-var 이름4 = 회원들[0];
+var 이름4 = 회원들[0]; //typescript에서는 any를 사용하지 않는 것이 좋다. (typescript 해제)
+var 이름5 = 회원들[0]; //unknown은 any보다는 좋다.
+//naraawing : 타입을 정확하게 명시할 수 없을 때 사용한다.
+function 내함수(x) {
+    if (typeof x === "number") {
+        return x * 2;
+    }
+    else {
+        return x + "hi";
+    }
+}
+function 내함수assertion(x) {
+    var array = [];
+    array[0] = x;
+}
+//assertion 사용 용도
+//1. Narrowing
+//2. 무슨 타입이 들어오는지 100% 확신할 때
+//상관없음
+내함수assertion('123');
+//평소 사용X 비상용
+var 동물;
+var 동물1 = {
+    name: "dog",
+    age: 3
+};
+//const 는 재할당만 막는 역할. 수정은 가능 
+var 출생지역 = { region: "seoul", country: "korea" };
+출생지역.region = "busan";
+var 여친 = { name: "kim" };
--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-let 이름 : {name?:string} = {name:"kim"};
+let 이름 : {name?:string,grade:string} = {grade:"A"};
 
 let 이름2:string|number = 123;
 
@@ -27,4 +27,62 @@ class User{
 
 let 회원들 = ["kim","park","lee",123,456,789];
 
-let 이름4:any = 회원들[0];
+let 이름4:any = 회원들[0]; //typescript에서는 any를 사용하지 않는 것이 좋다. (typescript 해제)
+let 이름5:unknown = 회원들[0] //unknown은 any보다는 좋다.
+
+//naraawing : 타입을 정확하게 명시할 수 없을 때 사용한다.
+function 내함수(x:number|string){
+    if(typeof x === "number"){
+        return x*2;
+    }else{
+        return x+"hi";
+    }
+}
+
+function 내함수assertion(x:number|string){
+    let array :number[] = [];
+    array[0] = x as number;
+}
+//assertion 사용 용도
+//1. Narrowing
+//2. 무슨 타입이 들어오는지 100% 확신할 때
+
+//상관없음
+내함수assertion('123');
+
+//평소 사용X 비상용
+
+let 동물 :string|number|undefined;
+
+type Animal = string|number|undefined;
+
+type Animal2 = {name:string,age:number}|undefined;
+let 동물1 : Animal2 = {
+    name:"dog",
+    age:3
+};
+
+//const 는 재할당만 막는 역할. 수정은 가능 
+const 출생지역 = {region:"seoul",country:"korea"};
+출생지역.region = "busan";
+
+type Girlfriend = {
+    //읽기 전용
+    readonly name:string,
+}
+
+const 여친:Girlfriend = {name:"kim"};
+//여친.name = "park"; Error!
+
+type Name1 = string;
+type Age = number;
+type Person = Name|Age;
+
+type PositionX = {x:number};
+//type PositionX = number Error!
+type PositionY = {y:number};
+
+//object 타입 합치기
+type NewType = PositionX&PositionY;
+
+ 
